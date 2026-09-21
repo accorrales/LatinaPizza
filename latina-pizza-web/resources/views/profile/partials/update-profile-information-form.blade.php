e5cc08e89065551e358225706645bcdc8080d4a9
accorrales <andercorrales28@gmail.com>
2026-09-20 21:50:25 -0600

    Make profile update flash CSP compatible

--- latina-pizza-web/resources/views/profile/partials/update-profile-information-form.blade.php
+++ latina-pizza-web/resources/views/profile/partials/update-profile-information-form.blade.php
@@ -1,21 +1,15 @@
 <section>
     <header>
-        <h2 class="text-lg font-medium text-gray-900">
-            {{ __('Profile Information') }}
-        </h2>
-
-        <p class="mt-1 text-sm text-gray-600">
-            {{ __("Update your account's profile information and email address.") }}
-        </p>
+        <h2 class="text-lg font-medium text-gray-900">{{ __('Profile Information') }}</h2>
+        <p class="mt-1 text-sm text-gray-600">{{ __("Update your account's profile information and email address.") }}</p>
     </header>
 
-    <form id="send-verification" method="post" action="{{ route('verification.send') }}">
+    <form id="send-verification" method="post" action="{{ route('verification.send') }}" data-show-loading>
         @csrf
     </form>
 
-    <form method="post" action="{{ route('profile.update') }}" class="mt-6 space-y-6">
-        @csrf
-        @method('patch')
+    <form method="post" action="{{ route('profile.update') }}" class="mt-6 space-y-6" data-show-loading>
+        @csrf @method('patch')
 
         <div>
             <x-input-label for="name" :value="__('Name')" />
@@ -32,16 +26,11 @@
                 <div>
                     <p class="text-sm mt-2 text-gray-800">
                         {{ __('Your email address is unverified.') }}
-
-                        <button form="send-verification" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                            {{ __('Click here to re-send the verification email.') }}
-                        </button>
+                        <button form="send-verification" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">{{ __('Click here to re-send the verification email.') }}</button>
                     </p>
 
                     @if (session('status') === 'verification-link-sent')
-                        <p class="mt-2 font-medium text-sm text-green-600">
-                            {{ __('A new verification link has been sent to your email address.') }}
-                        </p>
+                        <p class="mt-2 font-medium text-sm text-green-600">{{ __('A new verification link has been sent to your email address.') }}</p>
                     @endif
                 </div>
             @endif
@@ -49,15 +38,8 @@
 
         <div class="flex items-center gap-4">
             <x-primary-button>{{ __('Save') }}</x-primary-button>
-
             @if (session('status') === 'profile-updated')
-                <p
-                    x-data="{ show: true }"
-                    x-show="show"
-                    x-transition
-                    x-init="setTimeout(() => show = false, 2000)"
-                    class="text-sm text-gray-600"
-                >{{ __('Saved.') }}</p>
+                <p x-data="flashMessage" x-show="show" x-transition class="text-sm text-gray-600">{{ __('Saved.') }}</p>
             @endif
         </div>
     </form>
